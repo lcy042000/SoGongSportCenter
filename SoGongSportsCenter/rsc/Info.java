2c800c940d8de464b7836512721e5ec988b64665
lcy <8410294@naver.com>
2022-05-17 16:27:05 +0900

    init

---
+++ SoGongSportsCenter/rsc/Info.java
@@ -0,0 +1,5 @@
+public class Info {
+    public static String dbURL = "jdbc:mysql://localhost:3306/OOSE";
+    public static String dbId = "root";
+    public static String dbPassword = "db042000@";
+}
